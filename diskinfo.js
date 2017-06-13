2a904aac656d59dd63ca75df4b554d40bdb6c352
Marko Harjula <marko.harjula@gmail.com>
2017-06-13 15:37:40 +0300

    building scan function

--- diskinfo.js
+++ diskinfo.js
@@ -1,3 +1,21 @@
+const fs = require("fs");
+
+const partTypes = {
+	EMPTY:			0x00,
+	NTFS:			0x07,
+	LINUX_SWAP:		0x82,
+	LINUX:			0x83,
+	LINUX_EXTENDED:	0x85,
+	LINUX_LVM:		0x8e,
+	GTP:			0xee,
+	EFI:			0xef,
+	LINUX_RAID:		0xfd,
+	getName: function(val) { // print names for values
+		for (var k in partTypes) if (partTypes[k] == val) return k+' ('+val.toString(16)+')';
+		return '('+val.toString(16)+')';
+	}
+};
+module.exports.partTypes = partTypes;
 
 function parseMBRPartition(part) {
 	let out = {};
@@ -14,20 +32,34 @@ function parseMBR(mbr) {
 	if ( mbr.length < 512 || mbr[0x1fe] != 85 || mbr[0x1ff] != 170 ) { // MBR signature
 		throw Error('no MBR signature or buffer is less than 512 bytes');
 	}
-	let ret = {};
-	ret.gtp = false;
-	ret.partitions = [];
+	let ret = {partitions:[]};
 	if ( mbr[0x1bc] == 90 && mbr[0x1bc] == 90 ) { // 0x5A5A = copy protected (UEFI)
 		ret.copyProtected = true;
 	}
 	ret.uuid = mbr[0x1bb].toString(16) + mbr[0x1ba].toString(16) + mbr[0x1b9].toString(16) + mbr[0x1b8].toString(16); // DiskID: 1B8 (hex) through 1BE (hex) (looks like reverse)
-	for (var i = 446;i <= 508;i += 16) { // MBR table block
-		let data = parseMBRPartition(mbr.slice(i,i+16));
-		ret.partitions.push(data);
-		if ( data.type == 0xEE ) { // we have GTP type : https://www.win.tue.nl/~aeb/partitions/partition_types-1.html
-			ret.gtp = true;
-		}
-	}	
+	for (var i = 446;i <= 508;i += 16) { // MBR table blocks
+		ret.partitions.push( parseMBRPartition( mbr.slice(i,i+16) ) );
+	}
 	return ret;
 }
 module.exports.parseMBR = parseMBR;
+
+const buffer = Buffer.allocUnsafe(512);
+function scan(fd) {
+	if ( fs.readSync(fd,buffer,0,buffer.length,0) != 512 ) {
+		throw Error('read only partial data from MBR');
+	}
+	let rootMbr = parseMBR(buffer);
+	rootMbr.partitions.forEach(function(p){
+		if ( p.type == partTypes.EXTENDED ) { // TODO: Extended partition reading
+			
+		}
+		if ( p.type == partTypes.GTP ) { // TODO: GTP partition table reading
+			
+		}
+	});
+	return rootMbr;
+}
+module.exports.scan = scan;
+
+
